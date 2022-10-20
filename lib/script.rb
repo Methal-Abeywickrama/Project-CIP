696011918ac1e0447848d1941a718f162d6bd366
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2022-10-20 21:36:18 +0530

    Gradient and critical point calculation parts added

--- lib/script.rb
+++ lib/script.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-def find_coordinates(x_array=Array, y_array=Array)
+def find_coordinates(x_array, y_array)
   return if x_array.length != y_array.length
 
   xlength2 = x_array[-1] - x_array[0]
@@ -8,10 +8,10 @@ def find_coordinates(x_array=Array, y_array=Array)
   xmin = x_array[0]
   ymin = y_array[0]
   
-  x_array.map! { |x| ((x - xmin) * 90 / xlength2 ) }
-  y_array.map! { |y| ((y - ymin) * 130 /ylength2 ) }
+  new_x_array = x_array.map { |x| ((x - xmin) * 90 / xlength2 ) }
+  new_y_array = y_array.map { |y| ((y - ymin) * 130 /ylength2 ) }
 
-  pretty_print(format_coordinates(x_array, y_array))
+  pretty_print(format_coordinates(new_x_array, new_y_array))
 end
 
 def format_coordinates(x_array, y_array)
@@ -33,12 +33,29 @@ def pretty_print(array)
   end
 end
 
+def calculate_gradient(point_1, point_2, x_array, y_array)
+  x_range = x_array[-1] - x_array[0]
+  y_range = y_array[-1] - y_array[0]
+
+  point_a = [(point_1[0][0] * 5 + point_1[0][1]).to_f, (point_1[1][0] * 5 + point_1[1][1]).to_f]
+  point_b = [(point_2[0][0] * 5 + point_2[0][1]).to_f, (point_2[1][0] * 5 + point_2[1][1]).to_f]
+
+  y_difference = ((point_a[1] - point_b[1]) / 130) * y_range
+  x_difference = ((point_a[0] - point_b[0]) / 90) * x_range
+  x1 = ((point_a[0] / 90) * x_range + x_array[0]).floor(2)
+  y1 = ((point_a[1] / 130) * y_range + y_array[0]).floor(2)
+  x2 = ((point_b[0] / 90) * x_range + x_array[0]).floor(2)
+  y2 = ((point_b[1] / 130) * y_range + y_array[0]).floor(2)
+
+  puts 'The critical points are ...'
+  puts "Coordinate A #{x1}, #{y1}"
+  puts "Coordinate B #{x2}, #{y2}"
+  puts "The gradient is #{(y_difference / x_difference).floor(2)}"
+
+end
 
 y_hw = [8.8, 12.5, 13.8, 18.7, 20.3]
 x_hl = [11, 16.1, 17.6, 22.4, 25]
 
-a3 = [1, 2, 3, 4, 5]
-a4 = [6, 7, 8, 9, 10]
-
-
 find_coordinates(x_hl, y_hw)
+calculate_gradient([[0, 4], [1, 0]], [[17, 3], [25, 2]], x_hl, y_hw)
